8929ec0ada45f5f113a81776195a3519cc44fbc0
Souffle <k53715115@gmail.com>
2021-03-18 18:17:37 +0900

    Add comparison between Proposed scheme & Paper

--- WPBC_Single_User.m
+++ WPBC_Single_User.m
@@ -1,4 +1,3 @@
-% This file is optized for MATLAB Live Script
 %% 
 % *Summary*
 %% 
@@ -8,49 +7,50 @@
 
 Fc = 915e6;
 Ae = (3e8)^2 / (4*pi*Fc^2);  % Effective aperture of isotropic tag antenna
-K = 1;                      % Single User Case
+
 T = 200;                    % 200 symbol for coherence time
 M = 8;   % # of Transmit Antenna
 R = 4;   % # of Receive Antenna
+L = 4;    % pilot length per antenna
+K = 1 ;                      % Single User Case
+
+del = 0.25;
+eff = 0.65;    % rectifier efficiency
+fb_bit = 8;     % # of feedback bits    
+distance = 4;  % distance from reader
+
+noise_dBm = -20;  % dB Noise
+sigma = 1e-3 * 10 ^ (noise_dBm/10);            % Noise Power
 %% Orthogonal Pilot Sequence
 
-L = 4;    % pilot length per antenna
 D = L * M;                    % Pilot slot length
 T_WET = T - K * D;           % WET slot length
   
-
 X_pilot= eye(M);   
  
 X_T = repmat(sqrt(2) * X_pilot, 1, L);     % Transmit Power : 2 W  
 X = X_T.';
 %% Tag Model
-% $$\delta \;:\textrm{reflection}\;\textrm{coefficient}:\left\lbrack 0,1\right\rbrack 
-% \;1\to \textrm{reflect},0\to \textrm{absorb}$$
+% $$\delta \;:\mathrm{reflection}\;\mathrm{coefficient}:\left\lbrack 0,1\right\rbrack 
+% \;1\to \mathrm{reflect},0\to \mathrm{absorb}$$
 % 
-% $$\eta :\textrm{rectifier}\;\textrm{efficiency}$$
+% $$\eta :\mathrm{rectifier}\;\mathrm{efficiency}$$
 
-del = 0.25;
-eff = 0.65;    % rectifier efficiency
-fb_bit = 8;     % # of feedback bits      
 total_state = 2 ^ fb_bit;              % # of state
-distance = 4;  % distance from reader
 %% *Channel Model*
 
-PL = Ae / (4 * pi * distance^2);      % Path loss from Distance
+PL = Ae / (4 * pi * distance^2) ;     % Path loss from Distance
 sigma_h = sqrt(PL) * 1;               % Multi-path fading variance
-SNRdB = 100;  % dB
-sigma = 10 ^ -(SNRdB/10);            % Noise 
 
 % Fading Coefficient of F & B channel
-h_f = sigma_h / sqrt(2) * (randn(M, 1) + 1j * randn(M, 1))
-h_b = sigma_h / sqrt(2) * (randn(R, 1) + 1j * randn(R, 1))
+h_f = sigma_h / sqrt(2) * (randn(M, 1) + 1j * randn(M, 1));
+h_b = sigma_h / sqrt(2) * (randn(R, 1) + 1j * randn(R, 1));
 
 % Noise of F & B channel for each slot
 n = sigma / sqrt(2) * (randn(1, D) + 1j * randn(1, D));
 w = sigma / sqrt(2) * (randn(R, D) + 1j * randn(R, D));
 
 n_WET = sigma / sqrt(2) * (randn(1, T_WET) + 1j * randn(1, T_WET));
-w_WET = sigma / sqrt(2) * (randn(R, T_WET) + 1j * randn(R, T_WET));
 %% 
 %% Forward Channel
 % Received signal in Tag: $b={\left(h^{\;f} \right)}^{\;T} X+n\in C^{\;1\textrm{XD}}$
@@ -65,9 +65,9 @@ b = h_f.' * X_T + n;
 P_inc = abs(b).^ 2;     % Compensated incident power
 
 % Quantizing Incident Power in tag 
-bin = 1e-3 / (total_state - 1);
-partition = [bin: bin: 1e-3];
-codebook = [partition, 1.5e-3];
+bin = 6 * PL/ (total_state - 1);
+partition = [bin: bin: 6 * PL];
+codebook = [partition, 9 * PL];
 [idx, rn] = quantiz(P_inc, partition, codebook);     % rn = quantized incident power
 %% Backward Channel
 % Received Signal in HAP: $Y=\sqrt{\;\delta \;}h^{\;b} \;\left({\left(h^f \right)}^T 
@@ -81,16 +81,17 @@ codebook = [partition, 1.5e-3];
 Y = sqrt(del) * h_b * b + w;
 sigma_N = sqrt(R * sigma ^ 2 * (1 + del * sigma_h ^ 2));
 %% MMSE Estimation
-% $$\sigma {\;}_{\textrm{BS}}^2 =R\delta \;\sigma {\;}_h^4 \;$$
+% $$\sigma {\;}_{\mathrm{BS}}^2 =R\delta \;\sigma {\;}_h^4 \;$$
 % 
-% $$\hat{H} =\delta {\;}^{-\frac{1}{2}} \;Y_{\textrm{CE}} {X\left(R_{\textrm{hh}} 
-% X^T X+\sigma_N^2 I_M \right)}^{\;-1} R_{\textrm{hh}}$$
+% $$\hat{H} =\delta {\;}^{-\frac{1}{2}} \;Y_{\mathrm{CE}} {X\left(R_{\mathrm{hh}} 
+% X^T X+\sigma_N^2 I_M \right)}^{\;-1} R_{\mathrm{hh}}$$
 % 
-% $$\textrm{Error}\;\textrm{Covariance}:R_E ={\left(R_{\textrm{hh}}^{-1} +\delta 
-% \;R_{\textrm{NN}}^{-1} X^T X\right)}^{-1}$$ 
+% $$\mathrm{Error}\;\mathrm{Covariance}:R_E ={\left(R_{\mathrm{hh}}^{-1} +\delta 
+% \;R_{\mathrm{NN}}^{-1} X^T X\right)}^{-1}$$ 
 
 sigma_BS = sqrt(R * sigma_h ^ 4 * del);
-H_mmse = (del)^(-1/2) * Y * X * inv(sigma_BS * X_T * X + sigma_N^2 * eye(M)) * sigma_BS  % estimated BS-CSI
+
+H_mmse = (del)^(-1/2) * Y * X * inv(sigma_BS * X_T * X + sigma_N^2 * eye(M)) * sigma_BS % estimated BS-CSI
 H = h_b * h_f.'     % real BS-CSI
 
 MSE = 1 / (1 / (R * del * sigma_h ^ 4) + L * 1 / (sigma_N ^ 2 / del ));
@@ -121,20 +122,36 @@ phase_b_est = phase_b_tilda(:, 1);
 %% Energy Harvesting
 % Estimated path gain: $g^f ={\left(\hat{\left|h^f \right.} \left|\right.\right)}^2$
 % 
-% TX Beamforming: $\Phi =\frac{g^f }{\left|g^f \right|}$
+% TX Beamforming: $\Phi =\frac{g^f }{\left|g^f \right|}e^{{j\left({\hat{\Phi 
+% \;} }^f \right)}^* }$    -> Supply high power to good path & normalize to 1W
+% 
+% WET harvest power = $\eta \;\left(1-\delta \;\right)\cdot \left({\left|\left(h^f 
+% \right)\right.}^T \Phi {\;+\;n\left|\right.}^2 \right)\cdot \;\left(2W\right)$                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-Maximum_harvesting_power = 2 * PL * M * eff * (1-del)  % fading channel power가 1일 때 Maximum power
+TX_beamforming = mag_f_est / norm(mag_f_est) .* exp(1j * phase_f_est.');
+bs_1 = conj(H_mmse(1, :)) ./ norm(H_mmse(1, :));
+
+
+MAX_harvest = 2 * eff * (1 - del) * (h_f' * h_f)  % Maximum Harvestable power corresponds to fading channel
+BS_harvest = 2 * eff * (1 - del) * abs(bs_1 * h_f) .^ 2     % Just using BS channel
+WET_harvest = 2 * eff * (1 - del) * abs(TX_beamforming' * h_f) .^ 2     % Proposition
+
+
+WET_vs_BS = WET_harvest / BS_harvest    % larger than 1, Proposition is more efficient
+WET_vs_MAX = WET_harvest / MAX_harvest
+
+Pilot_harvest = 2 * eff * (1-del) * mean(P_inc);
+harvest_rate = ((T-T_WET) * Pilot_harvest + T_WET * WET_harvest) / T;
 %% Verification
 
 mag_f_real = abs(h_f);
 mag_b_real = abs(h_b);
 
-mag_f_acc = mag_f_est ./ mag_f_real;    % 1에 가까울 수록 정확하게 추정한 것
-mag_b_acc = mag_b_est ./ mag_b_real;    % 맞는지 잘 모르겠음 ... 맞는것같은데..... 왜 각 요소가 같징
+mag_f_acc = mag_f_est ./ mag_f_real    % Accurately estimated when accuracy closed to 1
+mag_b_acc = mag_b_est ./ mag_b_real ;   
 
 phase_f_real = angle(h_f);
 phase_b_real = angle(h_b);
 
-phase_f_acc = angle(exp(1j * phase_f_est.') ./ exp(1j * phase_f_real));  % 각 요소가 같으면 잘 allign된 것
+phase_f_acc = angle(exp(1j * phase_f_est.') ./ exp(1j * phase_f_real))  % Well alligned when each components are similar
 phase_b_acc = angle(exp(1j * phase_b_est) ./ exp(1j * phase_b_real));
